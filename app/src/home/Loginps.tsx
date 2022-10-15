d25986471596f72141c29c775384e509d12b03ed
Geovanna Rylari <109253952+Rylari@users.noreply.github.com>
2022-10-15 19:41:11 -0300

    Hi
    Nós somos foda

--- app/src/home/Loginps.tsx
+++ app/src/home/Loginps.tsx
@@ -4,21 +4,26 @@ import './css/Login.css'
 export default function Loginps() {
   return (
     <>
-      <div>Login</div>
-    
-    <div className="header">
-      <p className='conta'>May your life <br></br>
-      serve to save other <br></br>
+      <div className="header">
+      <p className='conta'>May your life<br></br>
+      serve to save other<br></br>
       lives
       </p>
-    <p>
-    if you don’t an account
+    <p className='conta1'>
+    if you don't an account<br></br>
     you can Register here!
     </p>
-      
-
     </div>
+    <div className='sombra'></div>
+    <img src={require('../img/medicos.png')} alt="" className="medico"/>
+    <input className="nome" type="text" placeholder='Nome:'></input>
+    <input className="email" type="text" placeholder='Enter Email:'></input>
+    <input className="senha" type="password" placeholder='**********'></input>
+    <input className="cpf" type="text" placeholder='CPF:'></input>
+    <input className="crp" type="text" placeholder='CRP:'></input>
+    <button>Sing In</button>
     </>
+    
   
   )
 }
